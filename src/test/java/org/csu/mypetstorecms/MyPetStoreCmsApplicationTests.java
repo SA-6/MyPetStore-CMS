53fb70c0ac4711d23c029bde31337cc14c73ef3b
WONDER <909785560@qq.com>
2024-03-18 08:45:07 +0800

    try push

---
+++ src/test/java/org/csu/mypetstorecms/MyPetStoreCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstorecms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetStoreCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
