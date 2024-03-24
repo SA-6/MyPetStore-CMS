b100ec43bbd4fc92fb18314b8b4eed5ddd7bcea7
WONDER <909785560@qq.com>
2024-03-24 20:27:01 +0800

    初步实现登录注册，实现了图形验证码

--- src/main/java/org/csu/mypetstorecms/MyPetStoreCmsApplication.java
+++ src/main/java/org/csu/mypetstorecms/MyPetStoreCmsApplication.java
@@ -1,9 +1,11 @@
 package org.csu.mypetstorecms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.csu.mypetstorecms.persistence")
 public class MyPetStoreCmsApplication {
 
     public static void main(String[] args) {
--- src/test/java/org/csu/mypetstorecms/MyPetStoreCmsApplicationTests.java
+++ src/test/java/org/csu/mypetstorecms/MyPetStoreCmsApplicationTests.java
@@ -1,13 +1,62 @@
 package org.csu.mypetstorecms;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.csu.mypetstorecms.entity.Admin;
+import org.csu.mypetstorecms.persistence.AdminMapper;
+import org.csu.mypetstorecms.service.AdminService;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@MapperScan("org.csu.mypetstorecms.persistence")
 class MyPetStoreCmsApplicationTests {
+    @Autowired
+    private AdminMapper adminMapper;
 
+    @Autowired
+    private AdminService adminService;
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void adminTest(){
+        Admin admin=new Admin();
+        admin.setUsername("cba");
+        admin.setPassword("321");
+        admin.setEmail("321@csu.edu.cn");
+        admin.setAddress("csu.railwayCampus");
+        admin.setPhone("12312312312");
+        admin.setRealName("lisi");
+        admin.setStatus(0);
+
+        int result = adminService.insertAdmin(admin);
+        if (result!=0) {
+            System.out.println("insert successful");
+        }
+        else {
+            System.out.println("insert failed");
+        }
+
+
+    }
+
+    @Test
+    void testLogin(){
+        String username = "abc";
+        String password = "123";
+
+        Admin admin = adminService.getAdminByUsernameAndPassword(username, password);
+        if(admin==null){
+            System.out.println("login failed");
+        }
+        else {
+            System.out.println("login successful");
+            System.out.println(admin);
+        }
+
     }
 
 }
